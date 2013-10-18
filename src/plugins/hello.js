8a2adab5178844b3b8deba66b698ed27985aa10b
Dan Lien <dan@danclien.com>
2013-10-17 21:59:06 -0500

    Adds console.log(argv) to hello.js sample plugin

--- src/plugins/hello.js
+++ src/plugins/hello.js
@@ -2,5 +2,6 @@ module.exports = {
   description: "Demo plugin that prints \"Hello, world!\"",
   run: function(argv) {
     console.log("Hello, world!");
+    console.log("argv: ", argv);
   } 
 };
